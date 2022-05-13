980c63ea5bc4474f10c3d2bb19390b00a8c75642
ebashirli <ebashirli@gmail.com>
2022-05-13 11:09:12 +0400

    adding async db connection; also improving code quality with black and flake8

--- main.py
+++ main.py
@@ -2,21 +2,34 @@ from fastapi import FastAPI
 from uvicorn import run
 from fastapi.middleware.cors import CORSMiddleware
 
-from api.files import router
+from api import files, users, courses, sections
+from db.db_setup import engine
+from db.models import user, course
 
-app = FastAPI()
+user.Base.metadata.create_all(bind=engine)
+course.Base.metadata.create_all(bind=engine)
 
-
-
-app.add_middleware(
-  CORSMiddleware,
-  allow_origins=["*"],
-  allow_credentials=True,
-  allow_methods=["*"],
-  allow_headers=["*"],
+app = FastAPI(
+    title="DOPS Backend",
+    discription="Drawing Office Processing System",
+    version="0.0.1",
+    contact={"name": "Elvin", "email": "ebashirli@hotmail.com"},
+    license_info={"name": "MIT"},
 )
 
-app.include_router(router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-if __name__ == '__main__':
-  run("main:app", host="0.0.0.0", port=5000, reload=True)
+app.include_router(files.router)
+app.include_router(users.router)
+app.include_router(courses.router)
+app.include_router(sections.router)
+
+if __name__ == "__main__":
+    # run("main:app", host="0.0.0.0", port=5000, reload=True)
+    run("main:app", reload=True)
